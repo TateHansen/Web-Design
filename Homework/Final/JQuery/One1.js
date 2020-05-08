0308b56d361c4fed566ace7c2bc8ff7c876d21ef
Tate Hansen <tah131@miami.edu>
2020-05-08 15:57:49 -0400

    Hi

---
+++ Homework/Final/JQuery/One1.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+$(".Hover1").hide();
+$(".Hover2").hide();
+$(".Hover3").hide();
+$(".Hover4").hide();
+$(".Hover6").hide();
+$(".Hover7").hide();
+$(".Hover8").hide();
+$(".Hover9").hide();
+$(".Hover10").hide();
+
+$(".number").mouseover(function(){
+  $(".Hover1").show();
+});
+$(".number").mouseover(function(){
+  $(".Hover2").show();
+});
+$(".number").mouseover(function(){
+  $(".Hover3").show();
+});
+$(".number").mouseover(function(){
+  $(".Hover4").show();
+});
+$(".number").mouseover(function(){
+  $(".Hover6").show();
+});
+$(".number").mouseover(function(){
+  $(".Hover7").show();
+});
+$(".number").mouseover(function(){
+  $(".Hover8").show();
+});
+$(".number").mouseover(function(){
+  $(".Hover9").show();
+});
+$(".number").mouseover(function(){
+  $(".Hover10").show();
+});
+
+});
+$("#ReadMore1").click(function(){
+  $(".Description1 p").html("In order to understand the importance of this moment one must understand the curse of the Billy Goat.The Curse of the Billy Goat was a sports-related curse that was supposedly placed on the Chicago Cubs Major League Baseball franchise in 1945, by Billy Goat Tavern owner William Sianis. The curse lasted 71 years, from 1945 to 2016. Because the odor of his pet goat, named Murphy, was bothering other fans, Sianis was asked to leave Wrigley Field, the Cubs home ballpark, during game 4 of the 1945 World Series Outraged, Sianis allegedly declared, Them Cubs, they ain't gonna win no more, which had been interpreted to mean that the Cubs would never win another National League (NL) pennant, at least for the remainder of Sianis's life.  The Cubs lost the 1945 World Series to the Detroit Tigers, and did not win a World Series championship again until 2016. The Cubs had last won the World Series in 1908. After the incident with Sianis and Murphy, the Cubs did not play in the World Series for the next 71 years until, on the 46th anniversary of William Sianis's death, the curse was broken when they defeated the Los Angeles Dodgers 5–0 in game 6 of the 2016 National League Championship Series to win the NL pennant.[4] The Cubs then defeated the American League (AL) champion Cleveland Indians 8–7 in 10 innings in game 7 to win the 2016 World Series, 108 years after their last win.");
+});
+
+
+
+
+
+
+
+});
